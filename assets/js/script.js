37a180af8ace8257d95ad827ef566596c5f16b40
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-03-04 17:05:53 +0100

    adjust form and inputs ids for proper login function listening to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
-// ** LOGIN LOGIC ** //
-// * AJAX to fetch user email & password
- fetch('assets/js/user_data.json')
-    .then(response => response.json())
-    .then(data => {
-        let email = data.info.email;
-        let password = data.info.password;
-        console.log(email);
-        console.log(password);
-    })
+// *** Let user data for global usage *** //
+let userData; 
 
+fetch('assets/js/user_data.json')
+    .then(response => {
+        if (!response.ok) throw new Error ('Failed to fetch user data');
+        return response.json();
+     })
+    .then(data => {
+        userData = data;
+        console.log(userData);
+    })
+    .catch(error => console.error('Fetch error', error));
+
+// ** LOGIN LOGIC ** //
+// ** get data from login form and compare
+    document.querySelector('#login-form').addEventListener('submit', function login(e) {
+        e.preventDefault();
+        let email = document.querySelector('#email').value;
+        let password = document.querySelector('#password').value;
+        
+
+    if (!userData) {
+        console.log('Data not loaded—try again');
+        return;
+    }
+    if (userData.info.email === email && userData.info.password === password) {
+        console.log('Login success:', userData.info.username);
+        window.location.href = 'dashboard.html'; // Redirect
+    } else {
+        console.log('Login failed');
+    };
+});
+
+
+    
 
 
   
